a925f83424b3d45a98d91666ea918c0bc0694f6a
Joery Vreijsen <joery.vreijsen@asml.com>
2024-12-02 16:43:48 +0100

    Day 1 part 2

--- src/main/kotlin/be/vreijsenj/aoc/days/Day01.kt
+++ src/main/kotlin/be/vreijsenj/aoc/days/Day01.kt
@@ -2,6 +2,7 @@ package be.vreijsenj.aoc.days
 
 import be.vreijsenj.aoc.utils.PuzzleUtils
 import kotlin.time.measureTime
+import kotlin.collections.getOrElse
 
 object Day01 {
 
@@ -11,8 +12,10 @@ object Day01 {
             val input = PuzzleUtils.getInput(1, 1)
 
             val resultPartOne = runPartOne(input);
+            val resultPartTwo = runPartTwo(input);
 
-            println("Sum of all calibration values (pt.1): $resultPartOne")
+            println("Total distance (pt.1): $resultPartOne")
+            println("Similarity score (pt.2): $resultPartTwo")
         }
 
         println("Took $elapsed")
@@ -22,12 +25,21 @@ object Day01 {
        val left = input.map { x -> toInt(x, 0) }.sorted()
        val right = input.map { x -> toInt(x, 1) }.sorted()
 
-       println(input)
-
        return left.zip(right)
         .map { (l, r) -> Math.abs(l - r) }
         .sum()
     }
+
+    fun runPartTwo(input: List<String>): Int {
+        val left = input.map { x -> toInt(x, 0) }
+        val right = input.map { x -> toInt(x, 1) }
+                         .groupingBy { it }
+                         .eachCount()
+ 
+        return left
+         .map { l -> l * right.getOrElse(l, { 0 }) }
+         .sum()
+     }
 
     fun toInt(input: String, index: Int): Int {
         return input.split("   ")[index].toInt()
--- src/test/groovy/be/vreijsenj/aoc/days/Day01Test.groovy
+++ src/test/groovy/be/vreijsenj/aoc/days/Day01Test.groovy
@@ -4,7 +4,7 @@ import spock.lang.Specification
 
 class Day01Test extends Specification {
 
-    def "returns sum of all distances, considering smallest left to right"() {
+    def "returns total distance between left and right"() {
         given: "the example puzzle input"
             def input = [
                 "3   4",
@@ -20,6 +20,24 @@ class Day01Test extends Specification {
 
         then: "the result matches the example answer"
             result == 11
+    }
+
+    def "returns similarity score between left and right"() {
+        given: "the example puzzle input"
+            def input = [
+                "3   4",
+                "4   3",
+                "2   5",
+                "1   3", 
+                "3   9",
+                "3   3"
+            ]
+
+        when: "the distance is calculated"
+            def result = new Day01().runPartTwo(input)
+
+        then: "the result matches the example answer"
+            result == 31
     }
 
 }
